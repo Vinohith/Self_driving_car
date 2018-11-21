ee08262db4a58598f14ff7b96749a65cd5a865b5
Vinohith <vinohith1998@gmail.com>
2018-11-21 23:14:28 +0530

    Update model.py

--- Behavioral_cloning/model.py
+++ Behavioral_cloning/model.py
@@ -104,4 +104,9 @@ if name = '__main__':
 	checkpointer = ModelCheckpoint('checkpoints/weights.{epoch:02d}-{val_loss:.3f}.hdf5')
 	logger = CSVLogger(filename='logs/history.csv')
 
-	
+	nvidia_net.fit_generator(generator=generate_data_batch(train_data, augment_data=True, bias=CONFIG['bias']),
+							samples_per_epoch=300*CONFIG['batchsize'],
+							nb_epoch=50,
+							validation_data=generate_data_batch(val_data, augment_data=False, bias=1.0),
+							nb_val_samples=100*CONFIG['batchsize'],
+							callbacks=[checkpointer, logger])
