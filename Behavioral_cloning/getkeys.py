b685c7e2fb559f26fab562fc239ae3eb3592b034
Vinohith <vinohith1998@gmail.com>
2018-11-23 01:51:58 +0530

    Add file getkeys.py

---
+++ Behavioral_cloning/getkeys.py
@@ -0,0 +1,27 @@
+# adapted from https://github.com/recantha/EduKit3-RC-Keyboard/blob/master/rc_keyboard.py
+ 
+import sys, termios, tty, os, time
+ 
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+ 
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+ 
+# button_delay = 0.2
+
+while True:
+	char = getch()
+	if char == 'w':
+		print('Forward')
+	elif char == 'a':
+		print('Left')
+	elif char == 'd':
+		print('Right')
+	else:
+		print('Invalid')
