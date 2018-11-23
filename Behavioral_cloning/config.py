e782553d14edebe783ae390e304b7e9b9e5e7126
Vinohith <vinohith1998@gmail.com>
2018-11-23 23:16:48 +0530

    Add file config.py

---
+++ Behavioral_cloning/config.py
@@ -0,0 +1,13 @@
+CONFIG = {
+    'batchsize': 512,
+    'input_width': 200,
+    'input_height': 66,
+    'input_channels': 3,
+    'delta_correction': 0.25,
+    'augmentation_steer_sigma': 0.2,
+    'augmentation_value_min': 0.2,
+    'augmentation_value_max': 1.5,
+    'bias': 0.8,
+    'crop_height': range(20, 140),
+    'steering_adjustment': 0.27
+}
