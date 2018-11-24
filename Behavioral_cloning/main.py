070d1c643124d0caadef19ec376b581e8adf10e6
Vinohith <vinohith1998@gmail.com>
2018-11-24 19:35:53 +0530

    Update code

--- Behavioral_cloning/main.py
+++ Behavioral_cloning/main.py
@@ -1,11 +1,13 @@
 from helperfunction import data_preperation, generator_data, generator_val
 from model import get_nvidiamodel
 from keras.callbacks import ModelCheckpoint, CSVLogger
-from config import *
+import math
+# from config import *
 
 
 if __name__ == '__main__':
 	X_train, y_train, X_val, y_val = data_preperation('data/driving_log.csv')
+	print(len(X_train), len(y_train))
 	data_generator = generator_data(X_train, y_train, CONFIG['batchsize'])
 	val_generator = generator_val(X_val, y_val, CONFIG['batchsize'])
 
@@ -13,14 +15,14 @@ if __name__ == '__main__':
 	nvidia_net.compile(optimizer = 'adam', loss = 'mse', metrics = ['accuracy'])
 
 	with open('logs/model.json', 'w') as f:
-		f.write(nvidia_net.to_json)
+		f.write(nvidia_net.to_json())
 
-	checkpointer = ModelCheckpoint('checkpoints/weights.{epoch:02d}-{val_loss:.3f}.hdf5')
-	logger = CSVLogger(filename='logs/history.csv')
+	# checkpointer = ModelCheckpoint('checkpoints/weights.{epoch:02d}-{val_loss:.3f}.hdf5')
+	# logger = CSVLogger(filename='logs/history.csv')
 
-	model.fit_generator(data_generator, samples_per_epoch = math.ceil(len(X_train)), 
-						nb_epoch = 25, validation_data = val_generator, nb_val_samples = len(X_val), 
-						callbacks = [checkpointer, logger])
+	print('Started training')
+	nvidia_net.fit_generator(data_generator, samples_per_epoch = math.ceil(len(X_train)), nb_epoch = 25, validation_data = val_generator, nb_val_samples = len(X_val))
+						# callbacks = [checkpointer, logger])
 	print('Done training')
 
 	nvidia_net.save_weights('model.h5')
